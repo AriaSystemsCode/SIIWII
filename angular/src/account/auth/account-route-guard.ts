747dfa317e7a7c3c221c5ae636b84aac5788f51b
Aria System <Aria_Account@hotmail.com>
2023-11-26 12:06:20 -0800

    Test Push 2

--- angular/src/account/auth/account-route-guard.ts
+++ angular/src/account/auth/account-route-guard.ts
@@ -9,7 +9,7 @@ export class AccountRouteGuard implements CanActivate {
     constructor(
         private _permissionChecker: PermissionCheckerService,
         private _router: Router,
-        private _sessionService: AppSessionService
+        private _sessionService: AppSessionService  
     ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
