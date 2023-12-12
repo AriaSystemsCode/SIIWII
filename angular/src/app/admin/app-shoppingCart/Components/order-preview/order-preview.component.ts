eba7d46e2d236f0eaa9bc5f4d650277594e89eaf
EsraaAhmed3101 <EsraaAhmed1012015@gmail.com>
2023-12-12 14:15:02 +0200

    I37-FE Dev MS1 &MS2

--- angular/src/app/admin/app-shoppingCart/Components/order-preview/order-preview.component.ts
+++ angular/src/app/admin/app-shoppingCart/Components/order-preview/order-preview.component.ts
@@ -4,6 +4,7 @@ import { Observable, catchError, finalize } from "rxjs";
 import { GetAppTransactionsForViewDto } from "@shared/service-proxies/service-proxies";
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { ProductCatalogueReportParams } from "@app/main/app-items/appitems-catalogue-report/models/product-Catalogue-Report-Params";
 
 
 @Component({
@@ -16,6 +17,11 @@ export class OrderPreviewComponent extends AppComponentBase implements OnInit, O
     @Input("transactionFormPath") transactionFormPath;
     pdfPath: SafeResourceUrl;
     input = document.querySelector('input[type=file]');
+
+    printInfoParam: ProductCatalogueReportParams = new ProductCatalogueReportParams();
+    reportUrl:string="";
+    invokeAction = '/DXXRDV';
+    @Input("orderId") orderId;
 
     constructor(
         injector: Injector,
@@ -33,17 +39,28 @@ export class OrderPreviewComponent extends AppComponentBase implements OnInit, O
             this.loadPdf();
     }
     loadPdf() {
-        fetch(this.transactionFormPath)
-            .then(response => response.blob())
-            .then(blob => {
-                this.pdfPath = this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
-                // const reader = new FileReader();
-                // reader.onload = (e) => {
-                //     const fileContent = e.target.result as ArrayBuffer;
-                //     const blob = new Blob([new Uint8Array(fileContent)], { type: 'application/pdf' });
-                //     this.pdfPath = this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
-                // };
-                // reader.readAsArrayBuffer(this.transactionFormPath);
-            });
+        this.printInfoParam.reportTemplateName = this.transactionReportTemplateName;
+        this.printInfoParam.TransactionId = this.orderId.toString();
+        //this.printInfoParam.orderType=this.appTransactionsForViewDto.transactionType== TransactionType.SalesOrder  ? "SO" : "PO";
+        this.printInfoParam.orderConfirmationRole = this.getTransactionRole(this.appTransactionsForViewDto.enteredByUserRole);
+        this.printInfoParam.saveToPDF = true;
+        this.printInfoParam.tenantId = this.appSession?.tenantId
+        this.printInfoParam.userId = this.appSession?.userId
+        this.reportUrl = this.printInfoParam.getReportUrl()
+
+        this.pdfPath = this.sanitizer.bypassSecurityTrustResourceUrl(this.transactionFormPath);
+
+        // fetch(this.transactionFormPath)
+        //     .then(response => response.blob())
+        //     .then(blob => {
+        //         this.pdfPath = this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
+        //         // const reader = new FileReader();
+        //         // reader.onload = (e) => {
+        //         //     const fileContent = e.target.result as ArrayBuffer;
+        //         //     const blob = new Blob([new Uint8Array(fileContent)], { type: 'application/pdf' });
+        //         //     this.pdfPath = this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
+        //         // };
+        //         // reader.readAsArrayBuffer(this.transactionFormPath);
+        //     });
     }
 }
