732d019d3e8a9b127344b42bb5a1db3b857836a3
EsraaAhmed3101 <EsraaAhmed1012015@gmail.com>
2024-01-24 16:57:02 +0200

    Fix Issue T-SII-20240119.0007

--- angular/src/app/main/app-items/appItems/components/create-or-edit-app-item.component.ts
+++ angular/src/app/main/app-items/appItems/components/create-or-edit-app-item.component.ts
@@ -1452,8 +1452,15 @@ export class CreateOrEditAppItemComponent
             );
             let modalRefData: AppEntityListDynamicModalComponent =
                 modalRef.content;
+                
+              if(extraAttr?.paginationSetting?.skipCount)
+               extraAttr.paginationSetting.skipCount = 0; 
+            
+               this.loadExtraDataLookupList(extraAttr);
+
             if (modalRefData.selectionDone)
                 extraAttr.selectedValues = modalRefData.selectedRecords;
+
             // if(extraAttr.acceptMultipleValues){ // multi selection
             //     const selectedValues : number[] =  extraAttr.selectedValues
             //     selectedValues.forEach(selectedValue => {
