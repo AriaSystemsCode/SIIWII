acb4bd51004732716dc5e7c7860ab26505b5a797
Mai <mai.m@DEV-GENDY>
2024-02-04 12:48:29 +0200

    T-SII-20240125.0001-Marketplace-Order information gets affected by removing the qty but doesn't remove the order price, Order by (Prepack)

--- angular/src/app/main/marketplace/marketplace-products/components/marketplace-view-product.component.ts
+++ angular/src/app/main/marketplace/marketplace-products/components/marketplace-view-product.component.ts
@@ -266,11 +266,13 @@ export class MarketplaceViewProductComponent
         orders.map((order: any) => {
             order.color.sizes.map((size) => {
                 if (this.productDetails.orderByPrePack) {
+                    
                     let multiby =
                         size.sizeRatio * order.color.sizes[0].orderedPrePacks;
                     let priceMultibly = multiby * size.price;
                     qty = qty + multiby;
                     price = price + priceMultibly;
+                    debugger
                 } else {
                     if (!size.orderedQty)
                       size.orderedQty = 0
@@ -377,6 +379,7 @@ export class MarketplaceViewProductComponent
                 this.orderSummary[orderIndex]?.color.sizes[0].orderedPrePacks;
             sum = sum + multiby;
         });
+
         return sum;
     }
 
