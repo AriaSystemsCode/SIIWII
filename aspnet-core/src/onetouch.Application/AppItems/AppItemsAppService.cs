e355a9e7b071c3fc1de586648e2d4a614d1a3b98
Aria System <Aria_Account@hotmail.com>
2024-03-03 03:46:14 -0800

    ShareSelectedProducts API

--- aspnet-core/src/onetouch.Application/AppItems/AppItemsAppService.cs
+++ aspnet-core/src/onetouch.Application/AppItems/AppItemsAppService.cs
@@ -3037,8 +3037,8 @@ namespace onetouch.AppItems
         { 
 
             long returnCount=0;
-            var apptemSelector = from o in  _appItemSelectorRepository.GetAll().Where(e => e.Key == key).ToList()
-                                 join i in  _appItemRepository.GetAll().ToList() on o.SelectedId equals i.Id into j
+            var apptemSelector = from o in  _appItemSelectorRepository.GetAll().Where(e => e.Key == key)
+                                 join i in  _appItemRepository.GetAll() on o.SelectedId equals i.Id into j
                                  from j1 in j select new { item= j1 } ;
 
             var selectedItems =  apptemSelector.ToList(); //3194a542-2d03-13c2-82f3-6914504839dd
@@ -3046,9 +3046,10 @@ namespace onetouch.AppItems
             {
                 using (UnitOfWorkManager.Current.DisableFilter(AbpDataFilters.MustHaveTenant, AbpDataFilters.MayHaveTenant))
                 {
-                    foreach (var itm in apptemSelector)
+                    
+                    foreach (var itm in selectedItems)
                     {
-                        var sharedItem = await _appMarketplaceItem.GetAll().Where(z => z.SSIN == itm.item.SSIN).FirstOrDefaultAsync();
+                        var sharedItem =  _appMarketplaceItem.GetAll().Where(z => z.SSIN == itm.item.SSIN).FirstOrDefault();
                         if (sharedItem == null)
                         {
                             returnCount++;
@@ -3056,6 +3057,7 @@ namespace onetouch.AppItems
                             shareOp.AppItemId = itm.item.Id;
                             shareOp.SharingLevel = 1;
                             shareOp.SyncProduct = false;
+                            shareOp.ItemSharing = new List<ItemSharingDto>();
                             await ShareProduct(shareOp);
                         }
                     }
