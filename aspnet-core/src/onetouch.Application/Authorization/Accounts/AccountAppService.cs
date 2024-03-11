edc9de2e7bc02157d738a546fb9a0a68e26f9b3d
Hassan.i <Hassan.i@DEV-HASSAN-10VM>
2024-03-11 23:14:48 +0200

    fix email

--- aspnet-core/src/onetouch.Application/Authorization/Accounts/AccountAppService.cs
+++ aspnet-core/src/onetouch.Application/Authorization/Accounts/AccountAppService.cs
@@ -105,11 +105,12 @@ namespace onetouch.Authorization.Accounts
 
         public async Task<RegisterOutput> Register(RegisterInput input)
         {
+            
             if (UseCaptchaOnRegistration())
             {
                 await RecaptchaValidator.ValidateAsync(input.CaptchaResponse);
             }
-
+          
             var user = await _userRegistrationManager.RegisterAsync(
                 input.Name,
                 input.Surname,
