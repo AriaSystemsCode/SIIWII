927a6102badbc8384da3a12a49ce99b7fa386a89
EsraaAhmed3101 <EsraaAhmed1012015@gmail.com>
2024-05-16 18:03:42 +0200

    Fix issue P-SII-20240403.0006

--- angular/src/app/admin/app-shoppingCart/Components/order-preview/order-preview.component.ts
+++ angular/src/app/admin/app-shoppingCart/Components/order-preview/order-preview.component.ts
@@ -1,38 +1,51 @@
-import { Component, Injector, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
+import { AfterViewInit, Component, Injector, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
 import { AppComponentBase } from "@shared/common/app-component-base";
-import { GetAppTransactionsForViewDto } from "@shared/service-proxies/service-proxies";
+import { AppTransactionServiceProxy, GetAppTransactionsForViewDto } from "@shared/service-proxies/service-proxies";
+import { finalize } from "rxjs";
 
 @Component({
     selector: "app-order-preview",
     templateUrl: "./order-preview.component.html",
     styleUrls: ["./order-preview.component.scss"],
 })
-export class OrderPreviewComponent extends AppComponentBase implements OnInit, OnChanges {
+export class OrderPreviewComponent extends AppComponentBase implements OnInit, OnChanges  , AfterViewInit{
     @Input("appTransactionsForViewDto") appTransactionsForViewDto: GetAppTransactionsForViewDto;
     @Input("orderConfirmationFile") orderConfirmationFile;
     @Input("transactionFormPath") transactionFormPath;
+    @Input("orderId") orderId;
     loadingError: boolean = false;
 
     constructor(
-        injector: Injector
+        injector: Injector,
+        private _AppTransactionServiceProxy: AppTransactionServiceProxy,
     ) {
         super(injector);
     }
     ngOnInit(): void {
-        this.loadPdf();
     }
     ngOnChanges(changes: SimpleChanges) {
         this.loadPdf();
     }
+    ngAfterViewInit(){
+        this.loadPdf();
+    }
     loadPdf() {
         this.loadingError = true;
-        var base64String = this.orderConfirmationFile
-        var pdfViewer = document.getElementById('pdfViewer') as HTMLIFrameElement;
-
-        if (base64String && pdfViewer) {
-            pdfViewer.src = 'data:application/pdf;base64,' + base64String;
-            this.loadingError = false;
-        }
+        this.showMainSpinner()
+        this._AppTransactionServiceProxy.getTransactionOrderConfirmation(this.orderId)
+        .pipe(finalize(() => {
+            this.hideMainSpinner()
+        }))
+        .subscribe((res) => {
+            var base64String =res;
+            var pdfViewer = document.getElementById('pdfViewer') as HTMLIFrameElement;
+    
+            if (base64String && pdfViewer) {
+                pdfViewer.src = 'data:application/pdf;base64,' + base64String;
+                this.loadingError = false;
+            }
+        });
+       
     }
 
 }
