d9993c657137f7bc9d9802d4dce4e421d2c7ad3c
Aria System <Aria_Account@hotmail.com>
2024-07-22 04:12:02 -0700

    Navigation to billing account page

--- angular/src/app/main/accountInfos/components/accountInfo.component.ts
+++ angular/src/app/main/accountInfos/components/accountInfo.component.ts
@@ -26,7 +26,7 @@ import { MembersListComponent } from '@app/main/members-list/components/members-
 import { ImageUploadComponentOutput } from '@app/shared/common/image-upload/image-upload.component';
 import { Paginator } from 'primeng/paginator';
 import { Console, log } from 'console';
-import { AccountBillingComponent } from './accountBilling/AccountBilling/accountbilling.component'; 
+
 @Component({
     selector:'app-account-info',
     templateUrl:'./AccountInfo.component.html',
@@ -618,10 +618,7 @@ export class AccountInfoComponent extends AppComponentBase implements OnInit, Af
               queryParamsHandling: 'merge', // remove to replace all query params by provided
             }
         );
-        if(number == this.accountInfoPageTabsEnum.AccountBilling)
-        {
-           this.__router.navigateByUrl('/accountbilling');
-        }
+        
     }
     triggerProfile($event?){
         if($event) $event.stopPropagation() //prevent event bubbling
