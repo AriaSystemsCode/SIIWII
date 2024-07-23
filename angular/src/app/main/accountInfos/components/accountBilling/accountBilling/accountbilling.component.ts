0fbe5af182e5d4fb9747d482bfea4d1d213ab0c7
Aria System <Aria_Account@hotmail.com>
2024-07-23 04:34:01 -0700

    Add navigation buttons of billing option

--- angular/src/app/main/accountInfos/components/accountBilling/accountBilling/accountbilling.component.ts
+++ angular/src/app/main/accountInfos/components/accountBilling/accountBilling/accountbilling.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import {BillingOptionsPageTabs} from '../../../models/Billing-options-page-tabs.enum';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AddOnsComponent } from '../components/add-ons/add-ons.component';
+import { TenantInvoicesComponent } from '../components/tenant-invoices/tenant-invoices.component';
+import { ActivityLogComponent } from '../components/activity-log/activity-log.component';
+import { PlansComponent } from '../components/plans/plans.component';
 @Component({
   selector: 'app-accountbilling',
   templateUrl: './accountbilling.component.html',
   styleUrls: ['./accountbilling.component.scss']
 })
 export class AccountBillingComponent {
-
+billingOptionsTabs = BillingOptionsPageTabs;
+__router: Router;
+currentTab: BillingOptionsPageTabs;
+openbillingpage(number:BillingOptionsPageTabs)
+{
+  this.currentTab = number;
+  //this.__router.navigate(["add-ons"]);
+}
 }
