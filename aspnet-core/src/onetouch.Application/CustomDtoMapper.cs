a580a9ee221920bb819d03742a847dc305600ba5
Hassan.i <Hassan.i@DEV-HASSAN-10VM>
2024-09-09 23:31:04 +0200

    Sync-I40

--- aspnet-core/src/onetouch.Application/CustomDtoMapper.cs
+++ aspnet-core/src/onetouch.Application/CustomDtoMapper.cs
@@ -117,6 +117,7 @@ using onetouch.AppMarketplaceItems;
 using onetouch.AppMarketplaceItemLists;
 using onetouch.AppMarketplaceItems.Dtos;
 using onetouch.AppMarketplaceMessages;
+using onetouch.AppMarketplaceContacts.Dtos;
 
 namespace onetouch
 {
@@ -402,6 +403,53 @@ namespace onetouch
                  .ForMember(d => d.CurrencyId, s => s.MapFrom(ss => ss.CurrencyId == 0 ? null : ss.CurrencyId))
                  .ForMember(d => d.LanguageId, s => s.MapFrom(ss => ss.LanguageId == 0 ? null : ss.LanguageId))
             .ReverseMap();
+
+
+            configuration.CreateMap<CreateOrEditMarketplaceAccountInfoDto, AppContact>()
+                .ForMember(d => d.AccountType, s => s.Ignore())
+                .ForMember(d => d.CurrencyId, s => s.MapFrom(ss => ss.CurrencyId == 0 ? null : ss.CurrencyId))
+                .ForMember(d => d.LanguageId, s => s.MapFrom(ss => ss.LanguageId == 0 ? null : ss.LanguageId))
+                ;
+
+            configuration.CreateMap<AppContact, CreateOrEditMarketplaceAccountInfoDto>()
+                .ForMember(d => d.AccountType, s => s.Ignore())
+                .ForMember(d => d.CurrencyId, s => s.MapFrom(ss => ss.CurrencyId == 0 ? null : ss.CurrencyId))
+                .ForMember(d => d.LanguageId, s => s.MapFrom(ss => ss.LanguageId == 0 ? null : ss.LanguageId))
+                .ForMember(d => d.ContactAddresses, s => s.MapFrom(ss => ss.AppContactAddresses))
+                ;
+
+            //AppMarketplaceContact
+            configuration.CreateMap<AppMarketplaceAddress, AppAddressDto>().ReverseMap();
+            configuration.CreateMap<AppMarketplaceAddress, AppAddress>().ReverseMap();
+
+            configuration.CreateMap<AppMarketplaceAddressDto, AppAddressDto>().ReverseMap();
+            configuration.CreateMap<AppMarketplaceAddressDto, AppAddress>().ReverseMap();
+            configuration.CreateMap<AppMarketplaceAddressDto, AppMarketplaceAddress>().ReverseMap();
+
+            configuration.CreateMap<AppMarketplaceContactAddressDto, AppContactAddress>();
+            configuration.CreateMap<AppMarketplaceContactAddressDto, AppMarketplaceContactAddress>();
+            configuration.CreateMap<AppContactAddress, AppMarketplaceContactAddress>().ReverseMap();
+
+            configuration.CreateMap<AppContactAddress, AppMarketplaceContactAddressDto>()
+            .ForMember(d => d.AddressFk, s => s.MapFrom(ss => ss.AddressFk));
+
+            configuration.CreateMap<CreateOrEditAccountInfoDto, AppMarketplaceContact>()
+              .ForMember(a => a.Code, b => b.MapFrom(ent => ent.SSIN))
+               ;
+
+            configuration.CreateMap<AppMarketplaceContact, AppContactDto>();
+            configuration.CreateMap<AppMarketplaceContact, AppContact>()
+            .ForMember(d => d.AppContactAddresses, s => s.MapFrom(ss => ss.ContactAddresses));
+            configuration.CreateMap<AppContact, AppMarketplaceContact>()
+                .ForMember(d => d.ContactAddresses, s => s.MapFrom(ss => ss.AppContactAddresses));
+
+            configuration.CreateMap<AppContactAddressDto, AppMarketplaceContactAddress>().ReverseMap();
+
+            configuration.CreateMap<CreateOrEditMarketplaceAccountInfoDto, AppMarketplaceContact>()
+              .ForMember(a => a.Code, b => b.MapFrom(ent => ent.SSIN))
+               ;
+
+
 
             configuration.CreateMap<AppContact, AccountSummaryDto>()
                 .ForMember(d => d.PhoneNumber, s => s.MapFrom(ss => ss.Phone1Number))
