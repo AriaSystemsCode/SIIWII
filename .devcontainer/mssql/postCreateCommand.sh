596235fcbd86d0cbc82694d1b47f7d8801f5882d
AriaSystemsCode <119807498+AriaSystemsCode@users.noreply.github.com>
2024-11-27 19:53:17 +0200

    Update postCreateCommand.sh

--- .devcontainer/mssql/postCreateCommand.sh
+++ .devcontainer/mssql/postCreateCommand.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Siiwii@2024" -Q "CREATE DATABASE TestDB"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Siiwii@2024 -Q "CREATE DATABASE TestDB"
