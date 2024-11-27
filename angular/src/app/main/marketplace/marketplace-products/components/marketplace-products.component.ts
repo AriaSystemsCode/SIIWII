9f8af3ab80fb87198cbec97d2ae35884034af882
EsraaAhmed3101 <EsraaAhmed1012015@gmail.com>
2024-11-27 10:12:58 +0200

    Fix issue T-SII-20241101.0026

--- angular/src/app/main/marketplace/marketplace-products/components/marketplace-products.component.ts
+++ angular/src/app/main/marketplace/marketplace-products/components/marketplace-products.component.ts
@@ -112,8 +112,8 @@ export class MarketplaceProductsComponent
                 });
         }
         this.sortingData = [
-            { label: "Product Name", value: "name" },
-            { label: "Product code", value: "code" },
+            { label: "Product Name", value: "name" }, 
+            { label: "Product code", value: "manufacturercode" },
             { label: "Price low to high", value: "price" },
             { label: "Price high to low", value: "price desc" },
         ];
