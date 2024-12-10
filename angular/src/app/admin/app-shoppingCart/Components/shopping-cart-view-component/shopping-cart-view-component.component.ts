9fcfce8b31b914a02cbf250e17eb8e65e380e872
menna-ramadan <mennaramadan569@gmail.com>
2024-12-10 17:17:53 +0200

    fix i45 issues

--- angular/src/app/admin/app-shoppingCart/Components/shopping-cart-view-component/shopping-cart-view-component.component.ts
+++ angular/src/app/admin/app-shoppingCart/Components/shopping-cart-view-component/shopping-cart-view-component.component.ts
@@ -114,6 +114,7 @@ conNew : boolean
 TempComp : boolean = false
 currentFilter: string = '';
 regenrate : boolean = false
+syncMsg : boolean = false
   constructor(
     injector: Injector,
     private _AppTransactionServiceProxy: AppTransactionServiceProxy,
@@ -407,6 +408,24 @@ cancelAddLine() {
   this.shoppingCartTreeNodes.pop(); // Removes the last item
   this.shoppingCartTreeNodes = [...this.shoppingCartTreeNodes];
   // Reset selections as needed
+}
+
+cancelsaveLine() {
+  this.addLine = true
+  this.showAddLine = false;
+  this.showSaveCancel = false;
+ 
+  this.addNewLinebtn = true
+ 
+    this.selectedVariation = '';
+    this.filterForm.controls['selectedQuantity']?.setValue(0);
+    this.filterForm.controls['selectedPrice']?.setValue(0);
+
+    this.selectedQuantity = 0;
+
+  this.selectedPrice = 0
+  this.amount = 0;
+  
 }
 
   
@@ -1225,6 +1244,7 @@ stopReport(event) {
       .pipe(finalize(() => {
         this.hideMainSpinner()
         this.getShoppingCartData();
+        this.syncMsg = true
 
       } ))
       .subscribe((res) => {
