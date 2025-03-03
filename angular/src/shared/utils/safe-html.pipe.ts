d9721041d50660426813b771fb538f75b504ebb2
menna-ramadan <95030671+menna-ramadan@users.noreply.github.com>
2025-03-03 13:06:02 +0200

    fix

--- angular/src/shared/utils/safe-html.pipe.ts
+++ angular/src/shared/utils/safe-html.pipe.ts
@@ -2,7 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({
-  name: 'safeHtml'
+  name: 'safeHtml',
+  
 })
 export class SafeHtmlPipe implements PipeTransform {
   constructor(private sanitized: DomSanitizer) {}
