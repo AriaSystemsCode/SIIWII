fc91338e1e5c5562456d0d1b808b68958f182dfb
menna-ramadan <mennaramadan569@gmail.com>
2025-04-08 10:05:38 -0700

    fix

--- angular/src/app/admin/appTransactions/appTransactions/createTransactionModal.component.ts
+++ angular/src/app/admin/appTransactions/appTransactions/createTransactionModal.component.ts
@@ -1238,33 +1238,6 @@ export class CreateTransactionModal extends AppComponentBase implements OnInit,O
         this.invalidSellerContactEMailAddress = "";
     }
 
-    cancel() {
-        this.isSeller = false;
-        this.isBuyer = false;
-        this.isBuyerTempAccount = false;
-        this.isSellerCompanyIdExist = false;
-        this.isCompantIdExist = false;
-        this.orderForm.controls["buyerCompanySSIN"].setValue('');
-        this.buyerCompanySSIN = ''
-        this.sellerCompanySSIN = ''
-        this.areSame = false
-        this.buyerComapnyId = 0
-        this.getAllCompanies();
-        this.sellerContacts = [];
-        this.buyerContacts = [];
-        this.orderForm.reset();
-        this.role = "";
-        this.modalClose.emit(false);
-        this.display = false;
-        this.Role.value = {};
-        this.submitted = false;
-        this.roles = [];
-        this.invalidSellerPhoneNumber = "";
-        this.invalidBuyerPhoneNumber = "";
-        this.invalidBuyerContactEMailAddress = "";
-        this.invalidSellerContactEMailAddress = "";
-
-    }
 
     changeTouchState(event){
         this.orderForm.controls['currencyId'].setValue(event.value)
@@ -1334,4 +1307,31 @@ export class CreateTransactionModal extends AppComponentBase implements OnInit,O
 
 
     }
+
+    cancel() {
+        this.isSeller = false;
+        this.isBuyer = false;
+        this.isBuyerTempAccount = false;
+        this.isSellerCompanyIdExist = false;
+        this.isCompantIdExist = false;
+        this.orderForm.controls["buyerCompanySSIN"].setValue('');
+        this.areSame = false
+        this.buyerComapnyId = 0
+        this.getAllCompanies();
+        this.sellerContacts = [];
+        this.buyerContacts = [];
+        this.orderForm.reset();
+        this.role = "";
+        this.modalClose.emit(false);
+        this.display = false;
+        this.Role.value = {};
+        this.submitted = false;
+        this.roles = [];
+        this.invalidSellerPhoneNumber = "";
+        this.invalidBuyerPhoneNumber = "";
+        this.invalidBuyerContactEMailAddress = "";
+        this.invalidSellerContactEMailAddress = "";
+
+    }
+
 }
