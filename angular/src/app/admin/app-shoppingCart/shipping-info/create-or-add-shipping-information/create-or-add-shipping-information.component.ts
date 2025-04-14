cf10573ee4eeec09cf18e656f847e92abd20983d
Mariam <Mariam@WEBAPP-DEV>
2025-04-14 12:46:32 -0700

    merge

--- angular/src/app/admin/app-shoppingCart/shipping-info/create-or-add-shipping-information/create-or-add-shipping-information.component.ts
+++ angular/src/app/admin/app-shoppingCart/shipping-info/create-or-add-shipping-information/create-or-add-shipping-information.component.ts
@@ -1,6 +1,6 @@
-import { Component, Injector, Input, OnInit, Output, EventEmitter, ViewChild, ViewChildren, SimpleChanges, OnChanges, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, Injector, Input, OnInit, Output, EventEmitter, ViewChild, ViewChildren, SimpleChanges, OnChanges, AfterViewInit } from '@angular/core';
 import { ShoppingCartoccordionTabs } from '../../Components/shopping-cart-view-component/ShoppingCartoccordionTabs';
-import { AppEntitiesServiceProxy, AppTransactionServiceProxy, GetAppTransactionsForViewDto, ContactRoleEnum, AppTransactionContactDto, AccountsServiceProxy } from '@shared/service-proxies/service-proxies';
+import { AppEntitiesServiceProxy, AppTransactionServiceProxy, GetAppTransactionsForViewDto, ContactRoleEnum, AppTransactionContactDto } from '@shared/service-proxies/service-proxies';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { finalize } from 'rxjs';
 import { AddressComponent } from '../../Components/address/address.component';
@@ -51,8 +51,6 @@ SuccessMsg: boolean = false;
     injector: Injector,
     private _AppTransactionServiceProxy: AppTransactionServiceProxy,
     private _appEntitiesServiceProxy: AppEntitiesServiceProxy,
-      private _AccountsServiceProxy: AccountsServiceProxy,
-      private cdRef: ChangeDetectorRef,
   ) {
     super(injector);
 
@@ -78,7 +76,7 @@ SuccessMsg: boolean = false;
   ngOnInit() {
     this.isMamualAcc()
     if(this.currentTab == ShoppingCartoccordionTabs.ShippingInfo){
-      this.GetContactDefaults()
+  
     this.oldappTransactionsForViewDto = JSON.parse(JSON.stringify(this.appTransactionsForViewDto));
     let shipFromObj = this.appTransactionsForViewDto?.appTransactionContacts?.filter(x => x.contactRole == ContactRoleEnum.ShipFromContact);
     shipFromObj[0]?.companySSIN && shipFromObj[0]?.contactAddressDetail?.addressLine1 ? this.shipFromSelectedAdd = shipFromObj[0]?.contactAddressDetail : null;
@@ -103,7 +101,6 @@ SuccessMsg: boolean = false;
       this.loadShipViaList();
     }
   }
-  this.checkValid()
   }
 
   updateTabInfo(addObj, contactRole) {
@@ -249,8 +246,6 @@ SuccessMsg: boolean = false;
       });
   }
 
-
-  
   enterStore() {
     this.appTransactionsForViewDto.buyerStore = this.storeVal;
   }
@@ -328,7 +323,6 @@ SuccessMsg: boolean = false;
             this.shipViaValue=this.shipViaList[0];
             this.appTransactionsForViewDto.shipViaId = this.shipViaValue?.value;
             this.appTransactionsForViewDto.shipViaCode = this.shipViaValue?.code;
-            this.cdRef.detectChanges();
         }else if(this.appTransactionsForViewDto.shipViaId){
           this.shipViaValue=this.shipViaList.filter(item=>item.value==this.appTransactionsForViewDto.shipViaId);
         }
@@ -344,7 +338,6 @@ SuccessMsg: boolean = false;
   }
   onUpdateAppTransactionsForViewDto($event) {
     this.appTransactionsForViewDto = $event;
-    
     this.validateShippingTab();
 
   }
@@ -393,24 +386,5 @@ this.validateShippingTab();
       $event == true ? this.validateShippingTab():''
     }
   }
-  GetContactDefaults(){
-    this._AccountsServiceProxy.getContactDefaults()
-      .subscribe((res) => {
-        if (!this.appTransactionsForViewDto.shipViaId) {
-          this.appTransactionsForViewDto.shipViaId = res.shipViaId; 
-          this.appTransactionsForViewDto.shipViaCode = res.shipViaCode; 
-          this.cdRef.detectChanges();
-        }
-      });
-  }
 
-  checkValid(){
-    if (this.enableSAveShipFrom && this.enableSAveShipTo &&this.appTransactionsForViewDto.shipViaId) {
-      this.shippingTabValid = true;
-      this.shippingInfOValid.emit(ShoppingCartoccordionTabs.ShippingInfo);
-    } else {
-      this.shippingTabValid = false;
-    }
-  }
-  
 }
