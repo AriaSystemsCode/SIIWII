801175209002c90cfc7142f4d800b82670127222
menna-ramadan <mennaramadan569@gmail.com>
2025-04-23 11:04:30 -0700

    fix dates

--- angular/src/app/admin/app-shoppingCart/shipping-info/create-or-add-shipping-information/create-or-add-shipping-information.component.ts
+++ angular/src/app/admin/app-shoppingCart/shipping-info/create-or-add-shipping-information/create-or-add-shipping-information.component.ts
@@ -205,16 +205,6 @@ SuccessMsg: boolean = false;
   }
   createOrEditTransaction() {
     this.showMainSpinner()
-    let enteredDate = this.appTransactionsForViewDto.enteredDate.toLocaleString();
-    let startDate = this.appTransactionsForViewDto.startDate.toLocaleString();
-    let availableDate = this.appTransactionsForViewDto.availableDate.toLocaleString();
-    let completeDate = this.appTransactionsForViewDto.completeDate.toLocaleString();
-
-
-    this.appTransactionsForViewDto.enteredDate = moment.utc(enteredDate);
-    this.appTransactionsForViewDto.startDate = moment.utc(startDate);
-    this.appTransactionsForViewDto.availableDate = moment.utc(availableDate);
-    this.appTransactionsForViewDto.completeDate = moment.utc(completeDate);
     this.appTransactionsForViewDto.timeZoneValue = Intl.DateTimeFormat().resolvedOptions().timeZone; 
     this._AppTransactionServiceProxy.createOrEditTransaction(this.appTransactionsForViewDto)
       .pipe(finalize(() => { this.hideMainSpinner();
