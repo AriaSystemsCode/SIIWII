a6725773b0b11894d424ea3fb5af463e075747e5
menna-ramadan <mennaramadan569@gmail.com>
2025-06-04 11:50:52 -0700

    fix acc

--- angular/src/app/main/accountInfos/components/accountInfo.component.ts
+++ angular/src/app/main/accountInfos/components/accountInfo.component.ts
@@ -797,9 +797,7 @@ export class AccountInfoComponent extends AppComponentBase implements OnInit, Af
                     this.handleComponentMode();
 
                 } else {
-                    // return this._router.navigate(['app/main/accounts'])
             this._router.navigate([`/app/main/account/view/${this.accountInfoTemp.id}`])
-            this.changeTab(AccountInfoPageTabs.ProfileView);
             this.changeTab(AccountInfoPageTabs.ProfileView);
             this.getAccountDataForView()
                 }
