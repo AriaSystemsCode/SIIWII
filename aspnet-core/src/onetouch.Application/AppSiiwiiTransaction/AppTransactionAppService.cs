df50a4809d0695c4658ed683c6373ef5f26f199c
Mariam <Mariam@WEBAPP-DEV>
2025-06-17 07:42:08 -0700

    Reomve classification and categories when transaction is discarded

--- aspnet-core/src/onetouch.Application/AppSiiwiiTransaction/AppTransactionAppService.cs
+++ aspnet-core/src/onetouch.Application/AppSiiwiiTransaction/AppTransactionAppService.cs
@@ -3432,6 +3432,8 @@ namespace onetouch.AppSiiwiiTransaction
                     //
                     await _appTransactionContactsRepository.DeleteAsync(s => s.TransactionId == transactionId);
                     await _appTransactionDetails.DeleteAsync(s => s.TransactionId == transactionId);
+                    await _appEntityCategoryRepository.DeleteAsync(z => z.EntityId == transactionId);
+                    await _appEntityClassificationRepository.DeleteAsync(z => z.EntityId == transactionId);
                     await CurrentUnitOfWork.SaveChangesAsync();
                 }
             }
