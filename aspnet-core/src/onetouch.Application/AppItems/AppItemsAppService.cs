af1e87dcf9249736a2d2dbc5719fcda7d974bc4c
Mariam <Mariam@WEBAPP-DEV>
2025-06-25 10:57:58 +0300

    change import item 2nd parameter

--- aspnet-core/src/onetouch.Application/AppItems/AppItemsAppService.cs
+++ aspnet-core/src/onetouch.Application/AppItems/AppItemsAppService.cs
@@ -5577,7 +5577,7 @@ namespace onetouch.AppItems
             return returnCategory;
         }
         
-        public async Task<List<ImportItemReturnDto>> ImportItem(List<ImportItemInputDto> itemExcelDtoList, ExcelRecordRepeateHandler repeatHandler)
+        public async Task<List<ImportItemReturnDto>> ImportItem(List<ImportItemInputDto> itemExcelDtoList, string repeatHandler)
         {
             AppItemExcelResultsDTO saveExcelinput = new AppItemExcelResultsDTO();
             saveExcelinput.CodesFromList = new List<string>();
@@ -5585,7 +5585,7 @@ namespace onetouch.AppItems
             saveExcelinput.FromList = new List<int>();
             saveExcelinput.ErrorMessage = "";
             saveExcelinput.ExcelRecords = new List<AppItemtExcelRecordDTO>();
-            saveExcelinput.RepreateHandler = repeatHandler;
+            saveExcelinput.RepreateHandler = (ExcelRecordRepeateHandler)Enum.Parse(typeof(ExcelRecordRepeateHandler), repeatHandler.ToString()); 
             saveExcelinput.To = 0;
             saveExcelinput.From = 0;
             List<ImportItemReturnDto> returnList = new List<ImportItemReturnDto>();
