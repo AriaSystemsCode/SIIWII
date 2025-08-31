3755f0148a30a15fa6f2207ad4e64ac4b4dbbeb6
HassanIbrahimAli <Hassan.I@ariasystem.sbiz>
2025-08-31 10:12:45 -0700

    fix BE

--- aspnet-core/src/onetouch.Web.Host/Startup/onetouchWebHostModule.cs
+++ aspnet-core/src/onetouch.Web.Host/Startup/onetouchWebHostModule.cs
@@ -8,6 +8,7 @@ using Abp.AspNetZeroCore.Web.Authentication.External.OpenIdConnect;
 using Abp.AspNetZeroCore.Web.Authentication.External.WsFederation;
 using Abp.Configuration.Startup;
 using Abp.Dependency;
+using Abp.Domain.Uow;
 using Abp.Modules;
 using Abp.Reflection.Extensions;
 using Abp.Threading.BackgroundWorkers;
@@ -20,9 +21,77 @@ using onetouch.Configuration;
 using onetouch.EntityFrameworkCore;
 using onetouch.MultiTenancy;
 using onetouch.Web.Controllers;
+using Abp.Configuration.Startup;
+using Abp.Domain.Uow;
+using Abp.MultiTenancy;
+using Abp.Runtime.Session;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Data.SqlClient;
 
 namespace onetouch.Web.Startup
 {
+
+
+    
+    public class OriginBasedConnectionStringResolver : DefaultConnectionStringResolver
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public OriginBasedConnectionStringResolver(
+            IAbpStartupConfiguration configuration,
+            IHttpContextAccessor httpContextAccessor
+        ) : base(configuration)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public override string GetNameOrConnectionString(ConnectionStringResolveArgs args)
+        {
+            var origin = _httpContextAccessor.HttpContext?.Items["XXXRequestOrigin"]?.ToString();
+            //return "Server=AriaSQL\\AriaNexus; Database=onetouchDevDb3;TrustServerCertificate=True; User=sa; Password=Aria@2021;";
+            //Aria.MASTER
+            if (!string.IsNullOrWhiteSpace(origin))
+            {
+                string AriaMasterConnection = "Server=WEBAPP-DEV\\SIIWII; Database=Aria.MASTER;TrustServerCertificate=True;User ID=sa;Password=Siiwii@2024;";
+                using (var conn = new SqlConnection(AriaMasterConnection))
+                {
+                    conn.Open();
+
+                    using (var cmd = new SqlCommand("SELECT TOP 1 ConnectionString FROM Clients WHERE Url = @Url", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Url", origin);
+
+                        var result = cmd.ExecuteScalar();
+                        if (result!=null && !string.IsNullOrEmpty(result.ToString()))
+                        {
+                            return result?.ToString();
+                        }
+                        else
+                        {
+                            return base.GetNameOrConnectionString(args);
+                        }
+                    }
+                }
+            }
+
+            //return "Server=WEBAPP-DEV\\SIIWII; Database=onetouchDevDb3_05192024;TrustServerCertificate=True;User ID=sa;Password=Siiwii@2024;";
+            //if (!string.IsNullOrEmpty(origin))
+            //{
+            //    if (origin.Contains("url1.com"))
+            //        return "Server=sqlserver1;Database=DB1;User Id=sa;Password=pass;";
+
+            //    if (origin.Contains("url2.com"))
+            //        return "Server=sqlserver2;Database=DB2;User Id=sa;Password=pass;";
+            //}
+
+            // fallback to default
+            return base.GetNameOrConnectionString(args);
+        }
+    }
+
+
     [DependsOn(
         typeof(onetouchWebCoreModule)
     )]
@@ -42,6 +111,11 @@ namespace onetouch.Web.Startup
         {
             Configuration.Modules.AbpWebCommon().MultiTenancy.DomainFormat = _appConfiguration["App:ServerRootAddress"] ?? "https://localhost:44303/";
             Configuration.Modules.AspNetZero().LicenseCode = _appConfiguration["AbpZeroLicenseCode"];
+
+          
+            IocManager.Register<IConnectionStringResolver, OriginBasedConnectionStringResolver>(Abp.Dependency.DependencyLifeStyle.Transient);
+        
+
         }
 
         public override void Initialize()
