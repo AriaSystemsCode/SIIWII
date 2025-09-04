43fb13ae0e708ccf9308582f3ea3f4f3a9a2abfe
esraa.a <esraa.ahmed31012015@gmail.com>
2025-09-04 14:04:18 +0200

    portal

--- aspnet-core/src/onetouch.EntityFrameworkCore/Migrations/20230430142127_AddITEMSSINIDToAppItemsListDetail.Designer.cs
+++ aspnet-core/src/onetouch.EntityFrameworkCore/Migrations/20230430142127_AddITEMSSINIDToAppItemsListDetail.Designer.cs
@@ -12,7 +12,7 @@ using onetouch.EntityFrameworkCore;
 namespace onetouch.Migrations
 {
     [DbContext(typeof(onetouchDbContext))]
-    [Migration("20230430142137_AddITEMSSINIDToAppItemsListDetail")]
+    [Migration("20230430142147_AddITEMSSINIDToAppItemsListDetail")]
     partial class AddITEMSSINIDToAppItemsListDetail
     {
         /// <inheritdoc />
