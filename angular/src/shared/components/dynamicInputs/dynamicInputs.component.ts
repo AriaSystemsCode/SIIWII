6e3c21846aaaaf2e11bb234d6305ff17d8fd4dea
esraa.a <esraa.ahmed31012015@gmail.com>
2025-11-28 00:00:24 +0200

    I49-F6-Setting upload & save images

--- angular/src/shared/components/dynamicInputs/dynamicInputs.component.ts
+++ angular/src/shared/components/dynamicInputs/dynamicInputs.component.ts
@@ -299,7 +299,7 @@ export class dynamicInputs extends AppComponentBase implements OnInit, OnChanges
 
     const blob = new Blob([file], { type: file.type });
      const originalName = file.name.split('|')[0];
-     const guid = file.name.split('|')[1];
+     const  guid = file.name.split('|')[1].split('.')[0]
       const newFile = new File([blob], originalName, { type: file.type });
         
       this.attachmentsUploader.addToQueue([newFile]);
