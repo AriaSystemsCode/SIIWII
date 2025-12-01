4010b30eb02443803eccf926dfd8b143c7947526
Mariam <Mariam@WEBAPP-DEV>
2025-12-01 12:08:06 +0200

    fix merge issue

--- aspnet-core/src/onetouch.Application/Helper/SystemTables.cs
+++ aspnet-core/src/onetouch.Application/Helper/SystemTables.cs
@@ -915,5 +915,25 @@ namespace onetouch.Helpers
             return obj;
         }
         //MMT40[End]
+        //I49[Start]
+        public async Task<long> GetObjectSectionId()
+        {
+            var obj = await _sycEntityObjectType.FirstOrDefaultAsync(x => x.Code == "MARKETPLACESECTION");
+            return obj.Id;
+        }
+        public async Task<long> GetObjectBlockId()
+        {
+            var obj = await _sycEntityObjectType.FirstOrDefaultAsync(x => x.Code == "MARKETPLACESECTIONBLOCK");
+            return obj.Id;
+        }
+        public async Task<long> GetEntityObjectStatusActiveLookup()
+        {
+            using (UnitOfWorkManager.Current.DisableFilter(AbpDataFilters.MustHaveTenant, AbpDataFilters.MayHaveTenant))
+            {
+                var obj = await _sycEntityObjectStatus.FirstOrDefaultAsync(x => x.Code == "ACTIVE" && x.ObjectCode == "LOOKUP");
+                return obj.Id;
+            }
+        }
+        //I49[End]
     }
 }
