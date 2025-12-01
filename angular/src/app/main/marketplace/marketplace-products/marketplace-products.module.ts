80eaae1d6256d55e53b950f266d255e85abb2220
menna-ramadan <mennaramadan569@gmail.com>
2025-12-01 13:16:33 +0200

    fix i49 merge

--- angular/src/app/main/marketplace/marketplace-products/marketplace-products.module.ts
+++ angular/src/app/main/marketplace/marketplace-products/marketplace-products.module.ts
@@ -66,6 +66,6 @@ import { OverALLRatingReviewsModule } from "@app/main/overallRating-reviews/over
         OverALLRatingReviewsModule
     ],
     providers:[AppMarketplaceItemsServiceProxy],
-    exports:[ProdcutCardComponent,ProdcutCardComponent]
+    exports:[ProdcutCardComponent,ProdcutCardComponent,MarketplaceProductsComponent]
 })
 export class MarketplaceProductsModule {}
