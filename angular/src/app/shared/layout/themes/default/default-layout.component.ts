29fd8866269a045816425be635aa04437658b935
menna-ramadan <mennaramadan569@gmail.com>
2025-12-03 02:49:52 +0200

    fix arabic issues

--- angular/src/app/shared/layout/themes/default/default-layout.component.ts
+++ angular/src/app/shared/layout/themes/default/default-layout.component.ts
@@ -68,10 +68,10 @@ export class DefaultLayoutComponent extends ThemesLayoutBaseComponent implements
         this._router.events
             .pipe(filter(event => event instanceof NavigationEnd))
             .subscribe(event => {this.currentRouteUrl = this._router.url.split(/[?#]/)[0]
-                console.log(this.currentRouteUrl,'urllllllllllllllll')
+             
                 const path = this.__router.url.split('?')[0]; // ignore query params
                 // show topbar for ANY route under /app/main/account/*
-                this.hideTopbar = !path.startsWith('/app/main/account/');
+                this.hideTopbar = !path.startsWith('/app/account/');
 
             });
     }
