1ccd20ecc9d48ffb16736f0393331dbb14cfe1a2
Mariam <Mariam@WEBAPP-DEV>
2026-01-27 13:35:58 +0200

    adjust ispublic flag

--- aspnet-core/src/onetouch.Application/AppEntities/AppEntitiesAppService.cs
+++ aspnet-core/src/onetouch.Application/AppEntities/AppEntitiesAppService.cs
@@ -1402,7 +1402,7 @@ namespace onetouch.AppEntities
                                     att = await _appAttachmentRepository.InsertAsync(att);
                                     await CurrentUnitOfWork.SaveChangesAsync();
                                     //entity.EntityAttachments.Add(new AppEntityAttachment { AttachmentCategoryId = (int)item.AttachmentCategoryId, EntityId = entity.Id, AttachmentId = att.Id });
-                                    entity.EntityAttachments.Add(new AppEntityAttachment { AttachmentCategoryId = (int)item.AttachmentCategoryId, EntityId = entity.Id, AttachmentId = att.Id, IsDefault = item.IsDefault, Attributes = item.Attributes });
+                                    entity.EntityAttachments.Add(new AppEntityAttachment { AttachmentCategoryId = (int)item.AttachmentCategoryId, EntityId = entity.Id, AttachmentId = att.Id, IsDefault = item.IsDefault, Attributes = item.Attributes,IsPublic = item.IsPublic });
                                     //await CurrentUnitOfWork.SaveChangesAsync();
                                 }
                                 else
