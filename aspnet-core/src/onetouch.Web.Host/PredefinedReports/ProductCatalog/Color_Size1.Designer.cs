b07b8b977e3928a698aee4be1e15582b1a13fce0
HassanIbrahimAli <Hassan.I@ariasystem.sbiz>
2026-01-31 13:34:12 -0800

    Fix aligment

--- aspnet-core/src/onetouch.Web.Host/PredefinedReports/ProductCatalog/Color_Size1.Designer.cs
+++ aspnet-core/src/onetouch.Web.Host/PredefinedReports/ProductCatalog/Color_Size1.Designer.cs
@@ -44,7 +44,6 @@
             this.TopMargin = new DevExpress.XtraReports.UI.TopMarginBand();
             this.BottomMargin = new DevExpress.XtraReports.UI.BottomMarginBand();
             this.Detail = new DevExpress.XtraReports.UI.DetailBand();
-            this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
             this.xrPictureBox1 = new DevExpress.XtraReports.UI.XRPictureBox();
             this.table2 = new DevExpress.XtraReports.UI.XRTable();
             this.tableRow2 = new DevExpress.XtraReports.UI.XRTableRow();
@@ -60,6 +59,7 @@
             this.itemListId = new DevExpress.XtraReports.Parameters.Parameter();
             this.onlyInStockColors = new DevExpress.XtraReports.Parameters.Parameter();
             this.mimimumInStockQty = new DevExpress.XtraReports.Parameters.Parameter();
+            this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
             ((System.ComponentModel.ISupportInitialize)(this.table2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             // 
@@ -112,23 +112,6 @@
             this.Detail.HierarchyPrintOptions.Indent = 50.8F;
             this.Detail.Name = "Detail";
             this.Detail.Scripts.OnBeforePrint = "Detail_BeforePrint_1";
-            // 
-            // xrLabel1
-            // 
-            this.xrLabel1.Dpi = 254F;
-            this.xrLabel1.ExpressionBindings.AddRange(new DevExpress.XtraReports.UI.ExpressionBinding[] {
-            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[Attachment]"),
-            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Visible", "[DataSource.CurrentRowIndex] =0\n")});
-            this.xrLabel1.Font = new DevExpress.Drawing.DXFont("Arial", 6F);
-            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(0F, 0F);
-            this.xrLabel1.Multiline = true;
-            this.xrLabel1.Name = "xrLabel1";
-            this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 5, 0, 0, 254F);
-            this.xrLabel1.SizeF = new System.Drawing.SizeF(71.30896F, 39.89917F);
-            this.xrLabel1.StylePriority.UseFont = false;
-            this.xrLabel1.StylePriority.UseTextAlignment = false;
-            this.xrLabel1.Text = "xrLabel1";
-            this.xrLabel1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
             // 
             // xrPictureBox1
             // 
@@ -316,6 +299,23 @@
             this.mimimumInStockQty.Type = typeof(int);
             this.mimimumInStockQty.ValueInfo = "0";
             // 
+            // xrLabel1
+            // 
+            this.xrLabel1.Dpi = 254F;
+            this.xrLabel1.ExpressionBindings.AddRange(new DevExpress.XtraReports.UI.ExpressionBinding[] {
+            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[Attachment]"),
+            new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Visible", "[DataSource.CurrentRowIndex] =0\n")});
+            this.xrLabel1.Font = new DevExpress.Drawing.DXFont("Arial", 6F);
+            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(0F, 0F);
+            this.xrLabel1.Multiline = true;
+            this.xrLabel1.Name = "xrLabel1";
+            this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 5, 0, 0, 254F);
+            this.xrLabel1.SizeF = new System.Drawing.SizeF(65F, 39.89917F);
+            this.xrLabel1.StylePriority.UseFont = false;
+            this.xrLabel1.StylePriority.UseTextAlignment = false;
+            this.xrLabel1.Text = "xrLabel1";
+            this.xrLabel1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
+            // 
             // Color_Size1
             // 
             this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
@@ -386,9 +386,9 @@
         private DevExpress.XtraReports.UI.XRControlStyle DetailData3_Odd;
         private DevExpress.XtraReports.UI.XRControlStyle PageInfo;
         private DevExpress.XtraReports.UI.XRPictureBox xrPictureBox1;
-        private DevExpress.XtraReports.UI.XRLabel xrLabel1;
         private DevExpress.XtraReports.Parameters.Parameter itemListId;
         private DevExpress.XtraReports.Parameters.Parameter onlyInStockColors;
         private DevExpress.XtraReports.Parameters.Parameter mimimumInStockQty;
+        private DevExpress.XtraReports.UI.XRLabel xrLabel1;
     }
 }
--- aspnet-core/src/onetouch.Web.Host/PredefinedReports/ProductCatalog/ProductsCatalogTemplate5.Designer.cs
+++ aspnet-core/src/onetouch.Web.Host/PredefinedReports/ProductCatalog/ProductsCatalogTemplate5.Designer.cs
@@ -52,6 +52,7 @@ namespace onetouch.Web.PredefinedReports.ProductCatalog {
 
             // Parameters
             this.to = reportInitializer.GetParameter("to");
+            this.muserCountry = reportInitializer.GetParameter("muserCountry");
             this.mimimumInStockQty = reportInitializer.GetParameter("mimimumInStockQty");
             this.onlyInStockColors = reportInitializer.GetParameter("onlyInStockColors");
             this.DetailPageGroupByName = reportInitializer.GetParameter("DetailPageGroupByName");
@@ -84,7 +85,6 @@ namespace onetouch.Web.PredefinedReports.ProductCatalog {
             this.selectedKey = reportInitializer.GetParameter("selectedKey");
             this.attachmentBaseUrl = reportInitializer.GetParameter("attachmentBaseUrl");
             this.attributeTypeId = reportInitializer.GetParameter("attributeTypeId");
-            this.muserCountry = reportInitializer.GetParameter("muserCountry");
 
             // Data Sources
             this.sqlDataSource1 = reportInitializer.GetDataSource<DevExpress.DataAccess.Sql.SqlDataSource>("sqlDataSource1");
@@ -134,6 +134,7 @@ namespace onetouch.Web.PredefinedReports.ProductCatalog {
         private DevExpress.XtraReports.UI.XRControlStyle DetailData3_Odd;
         private DevExpress.XtraReports.UI.XRControlStyle PageInfo;
         private DevExpress.XtraReports.Parameters.Parameter to;
+        private DevExpress.XtraReports.Parameters.Parameter muserCountry;
         private DevExpress.XtraReports.Parameters.Parameter mimimumInStockQty;
         private DevExpress.XtraReports.Parameters.Parameter onlyInStockColors;
         private DevExpress.XtraReports.Parameters.Parameter DetailPageGroupByName;
@@ -166,6 +167,5 @@ namespace onetouch.Web.PredefinedReports.ProductCatalog {
         private DevExpress.XtraReports.Parameters.Parameter selectedKey;
         private DevExpress.XtraReports.Parameters.Parameter attachmentBaseUrl;
         private DevExpress.XtraReports.Parameters.Parameter attributeTypeId;
-        private DevExpress.XtraReports.Parameters.Parameter muserCountry;
     }
 }
