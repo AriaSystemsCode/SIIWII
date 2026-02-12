912a744ae0550624d73fbb0b894e4781084dc144
menna-ramadan <mennaramadan569@gmail.com>
2026-02-12 21:50:46 +0200

    fix share btn

--- angular/src/shared/service-proxies/service-proxies.ts
+++ angular/src/shared/service-proxies/service-proxies.ts
@@ -15057,6 +15057,62 @@ export class AppItemsServiceProxy {
      * @param body (optional) 
      * @return Success
      */
+    addExtraAttrs(body: AppItemtExcelRecordDTO | undefined): Observable<AppItemtExcelRecordDTO> {
+        let url_ = this.baseUrl + "/api/services/app/AppItems/AddExtraAttrs";
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(body);
+
+        let options_ : any = {
+            body: content_,
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Content-Type": "application/json-patch+json",
+                "Accept": "text/plain"
+            })
+        };
+
+        return this.http.request("post", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processAddExtraAttrs(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processAddExtraAttrs(response_ as any);
+                } catch (e) {
+                    return _observableThrow(e) as any as Observable<AppItemtExcelRecordDTO>;
+                }
+            } else
+                return _observableThrow(response_) as any as Observable<AppItemtExcelRecordDTO>;
+        }));
+    }
+
+    protected processAddExtraAttrs(response: HttpResponseBase): Observable<AppItemtExcelRecordDTO> {
+        const status = response.status;
+        const responseBlob =
+            response instanceof HttpResponse ? response.body :
+            (response as any).error instanceof Blob ? (response as any).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = AppItemtExcelRecordDTO.fromJS(resultData200);
+            return _observableOf(result200);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf(null as any);
+    }
+
+    /**
+     * @param body (optional) 
+     * @return Success
+     */
     saveFromExcel(body: AppItemExcelResultsDTO | undefined): Observable<ExcelLogDto> {
         let url_ = this.baseUrl + "/api/services/app/AppItems/SaveFromExcel";
         url_ = url_.replace(/[?&]$/, "");
