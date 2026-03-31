915c6606654b01109940cd7c17ea958c6dd27dd6
esraa.a <esraa.ahmed31012015@gmail.com>
2026-03-31 02:10:06 +0200

    I49-FE Dev - Transaction charges

--- angular/src/app/app-entity-dynamic-modal/create-or-edit-app-entity-dynamic-modal/create-or-edit-app-entity-dynamic-modal.component.ts
+++ angular/src/app/app-entity-dynamic-modal/create-or-edit-app-entity-dynamic-modal/create-or-edit-app-entity-dynamic-modal.component.ts
@@ -797,14 +797,13 @@ isArabic: boolean = false;
     dropdownOptions(validEntries) {
         return validEntries.split('|');
     }
-    displayVisualTypes(): boolean {
+      displayVisualTypes(): boolean {
         //i49- what else ? 
-        if (this.entityObjectType.code.toString().toUpperCase() == "CHARGES")
-            this._displayVisualTypes = false;
-        else
-            this._displayVisualTypes = true;
+        const hiddenTypes = ['CHARGES', 'TRANSACTIONCHARGES','SHIPVIA', 'CHARGETYPES'];
 
-
+        this._displayVisualTypes = !hiddenTypes.includes(
+    this.entityObjectType.code.toString().toUpperCase()
+);
         return this._displayVisualTypes;
     }
 
