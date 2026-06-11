e2c90b01d82b4203fb762a97a0efd192dbbd36a3
menna-ramadan <mennaramadan569@gmail.com>
2026-06-12 02:28:46 +0300

    fix browse linesheet item

--- angular/src/app/main/app-items/app-items-browse/components/app-item-card.component.ts
+++ angular/src/app/main/app-items/app-items-browse/components/app-item-card.component.ts
@@ -47,13 +47,18 @@ export class AppItemCardComponent extends AppComponentBase {
     @Input()   sycAttachmentCategoryImage :SycAttachmentCategoryDto
     @Input()   acceptedAspectRatio
 
-     @Input() isArabic:boolean = false
+        currentLang:string
+    isArabic:boolean = true
     constructor(
         injector: Injector,
     ) {
         super(injector);
     }
 
+      ngOnInit(): void {
+                            this.currentLang = abp.utils.getCookieValue('Abp.Localization.CultureName')
+        this.currentLang == 'ar' || this.currentLang == 'ar-EG'  ? this.isArabic = true : this.isArabic = false
+    }
 
     eventHandler($event:ActionsMenuEventEmitter<AppItemBrowseEvents>){
         console.log(">>", 'publish')
