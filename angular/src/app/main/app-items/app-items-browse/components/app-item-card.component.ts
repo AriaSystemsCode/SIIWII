65566e275d38d9d64f7e2b996ee1539461e21d70
Mariam <Mariam@WEBAPP-DEV>
2026-06-16 11:28:36 +0300

    merge

--- angular/src/app/main/app-items/app-items-browse/components/app-item-card.component.ts
+++ angular/src/app/main/app-items/app-items-browse/components/app-item-card.component.ts
@@ -57,14 +57,8 @@ export class AppItemCardComponent extends AppComponentBase {
     ngOnInit(){
         this.currentLang = abp.utils.getCookieValue('Abp.Localization.CultureName')
         this.currentLang == 'ar' || this.currentLang == 'ar-EG'  ? this.isArabic = true : this.isArabic = false
-
-
     }
 
-      ngOnInit(): void {
-                            this.currentLang = abp.utils.getCookieValue('Abp.Localization.CultureName')
-        this.currentLang == 'ar' || this.currentLang == 'ar-EG'  ? this.isArabic = true : this.isArabic = false
-    }
 
     eventHandler($event:ActionsMenuEventEmitter<AppItemBrowseEvents>){
         console.log(">>", 'publish')
