53abb5e59747d3dd94ad4a045efc971be71a7f00
esraa.a <esraa.ahmed31012015@gmail.com>
2026-07-08 00:12:42 +0300

    Merge branch 'i49-change-req' of https://github.com/AriaSystemsCode/SIIWII into i49-change-req

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { LOCALE_ID } from '@angular/core';
 
 export function getRemoteServiceBaseUrl(): string {
-    return 'http://192.168.1.148:4240';
+    return 'http://192.168.1.148:4241';
 }
 
 describe('App: onetouch', () => {
