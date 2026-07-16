9f24c8be123cc7edf299e9fdcd1a594593736426
work_hassan@hotmail.com <hassan.i@ariasystems.biz>
2026-07-16 23:17:44 +0300

    P-SII-20251121.0001

--- angular/src/app/categories/select-categories-dynamic-modal.component.ts
+++ angular/src/app/categories/select-categories-dynamic-modal.component.ts
@@ -263,6 +263,7 @@ export class SelectCategoriesDynamicModalComponent
             this.isDepartment,
             this.entityId,    
             [],               
+            false,     
             undefined,     
             this.sortBy,     
             this.skipCount,
