25c3da2cead318741d54185b0497d50ff28267f7
menna-ramadan <mennaramadan569@gmail.com>
2026-08-19 13:56:38 +0300

    add code

--- angular/src/app/selectAddress/create-or-edit-Address-modal/create-or-edit-Address-modal.component.ts
+++ angular/src/app/selectAddress/create-or-edit-Address-modal/create-or-edit-Address-modal.component.ts
@@ -5,7 +5,8 @@ import {
     AccountsServiceProxy,
     LookupLabelDto,
     AppEntitiesServiceProxy,
-    AppAddressDto
+    AppAddressDto,
+    SycIdentifierDefinitionsServiceProxy
 } from '@shared/service-proxies/service-proxies';
 import { AppComponentBase } from '@shared/common/app-component-base';
 
@@ -39,6 +40,8 @@ export class CreateOrEditAddressModalComponent extends AppComponentBase {
         injector: Injector,
         private _accountsServiceProxy: AccountsServiceProxy,
         private _AppEntitiesServiceProxy: AppEntitiesServiceProxy,
+            private _sycIdentifierDefinitionsServiceProxy:
+        SycIdentifierDefinitionsServiceProxy
     ) {
         super(injector);
 
@@ -47,43 +50,158 @@ export class CreateOrEditAddressModalComponent extends AppComponentBase {
                 this.currentLang = abp.utils.getCookieValue('Abp.Localization.CultureName')
         this.currentLang == 'ar' || this.currentLang == 'ar-EG'  ? this.isArabic = true : this.isArabic = false
     }
-    show(addressId?: number, branch?: any, accountId?: number): void {
-        this.branchId = branch?.node?.data?.branch?.id
+    // show(addressId?: number, branch?: any, accountId?: number): void {
+    //     this.branchId = branch?.node?.data?.branch?.id
 
-        if (!addressId) {
-            this.address = new AppAddressDto();
-            this.address.accountId = accountId
-            this.active = true;
-            this.modal.show();
-        } else {
-            this._accountsServiceProxy.getAddressForEdit(addressId).subscribe(result => {
-                this.address = result;
-                var subCode = this.address.code.indexOf("-");
-                if (subCode >= 0)
-                    this.addressCode = this.address.code.substring(subCode + 1, this.address.code.length);
-                else
-                    this.addressCode = this.address.code
+    //     if (!addressId) {
+    //         this.address = new AppAddressDto();
+    //         this.address.accountId = accountId
+    //         this.active = true;
+    //         this.modal.show();
+    //     } else {
+    //         this._accountsServiceProxy.getAddressForEdit(addressId).subscribe(result => {
+    //             this.address = result;
+    //             var subCode = this.address.code.indexOf("-");
+    //             if (subCode >= 0)
+    //                 this.addressCode = this.address.code.substring(subCode + 1, this.address.code.length);
+    //             else
+    //                 this.addressCode = this.address.code
+    //             this.active = true;
+    //             this.modal.show();
+    //         });
+    //     }
+
+    //     this._AppEntitiesServiceProxy.getAllCountryForTableDropdown().subscribe(result => {
+    //         this.allCountries = result;
+    //         let noneOption: LookupLabelDto = Object.assign(new LookupLabelDto(), { label: "None", value: null })
+    //         this.allCountries.unshift(noneOption)
+    //     });
+    //     if(this.address.id == null || this.address.id == undefined || this.address.id == 0){
+    //         this._accountsServiceProxy.getAccountForView(this.appSession?.user?.accountId,5)
+    //         .subscribe((result)=>{ 
+         
+    //             this.address.countryId = result?.account?.countryId
+          
+    //         });
+   
+    //     }
+
+    // }
+async show(
+    addressId?: number,
+    branch?: any,
+    accountId?: number
+): Promise<void> {
+
+    this.branchId =
+        branch?.node?.data?.branch?.id;
+
+    this.addressCode = '';
+
+    if (!addressId) {
+
+        this.address =
+            new AppAddressDto();
+
+        this.address.accountId =
+            accountId;
+
+        await this.setNextAddressCode();
+
+        this.active = true;
+        this.modal.show();
+
+    } else {
+
+        this._accountsServiceProxy
+            .getAddressForEdit(addressId)
+            .subscribe(result => {
+
+                this.address =
+                    result;
+
+                const subCode =
+                    this.address.code
+                        ?.indexOf('-');
+
+                if (
+                    subCode !== undefined &&
+                    subCode >= 0
+                ) {
+                    this.addressCode =
+                        this.address.code.substring(
+                            subCode + 1
+                        );
+                } else {
+                    this.addressCode =
+                        this.address.code;
+                }
+
                 this.active = true;
                 this.modal.show();
             });
-        }
-
-        this._AppEntitiesServiceProxy.getAllCountryForTableDropdown().subscribe(result => {
-            this.allCountries = result;
-            let noneOption: LookupLabelDto = Object.assign(new LookupLabelDto(), { label: "None", value: null })
-            this.allCountries.unshift(noneOption)
-        });
-        if(this.address.id == null || this.address.id == undefined || this.address.id == 0){
-            this._accountsServiceProxy.getAccountForView(this.appSession?.user?.accountId,5)
-            .subscribe((result)=>{ 
-         
-                this.address.countryId = result?.account?.countryId
-          
-            });
-   
-        }
-
     }
+
+    this._AppEntitiesServiceProxy
+        .getAllCountryForTableDropdown()
+        .subscribe(result => {
+
+            this.allCountries =
+                result;
+
+            const noneOption:
+                LookupLabelDto =
+                Object.assign(
+                    new LookupLabelDto(),
+                    {
+                        label: 'None',
+                        value: null
+                    }
+                );
+
+            this.allCountries.unshift(
+                noneOption
+            );
+        });
+
+    if (!addressId) {
+
+        this._accountsServiceProxy
+            .getAccountForView(
+                this.appSession?.user?.accountId,
+                5
+            )
+            .subscribe(result => {
+
+                this.address.countryId =
+                    result?.account?.countryId;
+            });
+    }
+}
+    private async setNextAddressCode(): Promise<void> {
+    try {
+        const typeCode = this.entityObjectType; // "ADDRESS"
+
+        const nextCode =
+            await this._sycIdentifierDefinitionsServiceProxy
+                .getNextEntityCode(
+                    typeCode,
+                    this.appSession.tenantId
+                )
+                .toPromise();
+
+        this.addressCode =
+            nextCode?.toString() ?? '';
+
+    } catch (error) {
+        console.error(
+            'Error getting next address code',
+            error
+        );
+
+        this.addressCode = '';
+    }
+}
 
     save() {
         this.saving = true;
