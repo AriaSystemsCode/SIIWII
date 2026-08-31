d64a7724cc0f232a2b3538199aedd06be33c7a5e
work_hassan@hotmail.com <hassan.i@ariasystems.biz>
2026-08-31 23:25:12 +0300

    T-SII-20260821.0001

--- aspnet-core/src/onetouch.Web.Host/PredefinedReports/ProductCatalog/Color_Size.cs
+++ aspnet-core/src/onetouch.Web.Host/PredefinedReports/ProductCatalog/Color_Size.cs
@@ -21,21 +21,41 @@ namespace onetouch.Web.PredefinedReports.ProductCatalog
             tableCell25.ExpressionBindings.Add(
                 new ExpressionBinding("BeforePrint", "Text", "Trim([AttributeValue])"));
             tableCell25.Font = new DevExpress.Drawing.DXFont("Arial", 9F);
-            tableCell25.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 254F);
+            tableCell25.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 0, 0, 0, 254F);
             tableCell25.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft;
             tableCell25.TextFitMode = TextFitMode.ShrinkOnly;
             tableCell25.WordWrap = false;
+
+            for (var index = table2.ExpressionBindings.Count - 1; index >= 0; index--)
+            {
+                if (table2.ExpressionBindings[index].PropertyName == "LeftF")
+                {
+                    table2.ExpressionBindings.RemoveAt(index);
+                }
+            }
+
+            table2.ExpressionBindings.Add(new ExpressionBinding(
+                "BeforePrint",
+                "LeftF",
+                "iif(?showNo=0,15,iif(Min([counter],?showNo)<5,300,iif(Min([counter],?showNo)<6,150,15)))"));
         }
+
         private void ApplyDynamicSizeScaleFormatting(object sender, CancelEventArgs e)
         {
             var requestedSizeCount = Convert.ToInt32(showNo.Value);
             var availableSizeCount = Convert.ToInt32(GetCurrentColumnValue("counter"));
             var sizeCount = Math.Min(requestedSizeCount, availableSizeCount);
 
-            var fontSize = sizeCount <= 4 ? 10F
-                : sizeCount <= 6 ? 9F
-                : sizeCount <= 8 ? 8F
-                : 7F;
+            var fontSize = sizeCount <= 4 ? 12F
+                : sizeCount <= 6 ? 11F
+                : sizeCount <= 8 ? 10F
+                : 9F;
+
+            tableCell26.Font = new DevExpress.Drawing.DXFont("Arial", fontSize);
+            tableCell26.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 254F);
+            tableCell26.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
+            tableCell26.TextFitMode = TextFitMode.ShrinkOnly;
+            tableCell26.WordWrap = false;
 
             XRTableCell[] sizeCells =
             {
