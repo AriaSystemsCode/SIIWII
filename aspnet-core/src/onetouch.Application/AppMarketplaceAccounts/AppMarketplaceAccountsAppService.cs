a6bfb60bdf52bc5cfd58477fbf2f480206aee1ef
Mariam <Mariam@WEBAPP-DEV>
2026-09-14 07:46:08 -0700

    T-sii-20260817-00335

--- aspnet-core/src/onetouch.Application/AppMarketplaceAccounts/AppMarketplaceAccountsAppService.cs
+++ aspnet-core/src/onetouch.Application/AppMarketplaceAccounts/AppMarketplaceAccountsAppService.cs
@@ -391,7 +391,9 @@ namespace onetouch.AppMarketplaceAccounts
 
                     foreach (var account in accountsList)
                     {
-                        if (currentTenantAccountObject.SSIN == account.Account.SSIN)
+                        if (account==null || account.Account==null || account.Account.SSIN==null)
+                            continue;
+                        if (currentTenantAccountObject!= null && currentTenantAccountObject.SSIN == account.Account.SSIN)
                             continue;
                         //I50[Start]
                         var relationshipsQuery = _appContactRelationshipInfoRepository
