a6d473f7d1dcf7245fe2998cbeb3b8f6aa00915a
work_hassan@hotmail.com <hassan.i@ariasystems.biz>
2026-09-22 00:00:20 +0300

    Apply report layout and import fixes

--- aspnet-core/src/onetouch.Web.Host/PredefinedReports/ProductCatalog/Color_Size1.cs
+++ aspnet-core/src/onetouch.Web.Host/PredefinedReports/ProductCatalog/Color_Size1.cs
@@ -11,6 +11,15 @@ namespace onetouch.Web.PredefinedReports.ProductCatalog
         public Color_Size1()
         {
             InitializeComponent();
+            PageWidth = 612;
+            PageHeight = 450;
+        table2.LocationFloat = new DevExpress.Utils.PointFloat(71.30896F, 0F);
+        table2.SizeF = new System.Drawing.SizeF(526.6682F, 39.60747F);
+        tableCell25.Font = new DevExpress.Drawing.DXFont("Arial", 10F);
+        tableCell25.StylePriority.UseFont = true;
+        tableCell34.Padding = new DevExpress.XtraPrinting.PaddingInfo(1, 1, 0, 0, 254F);
+        tableCell34.StylePriority.UseTextAlignment = true;
+        tableCell34.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
         }
     }
 }
